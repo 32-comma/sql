6a42ce0dd5fb37a4b8981b5bfa059d0e18cab0b6
32-comma <louis.develog@gmail.com>
2025-03-23 13:33:24 +0900

    update

---
+++ mariaDB/grant.sql
@@ -0,0 +1,10 @@
+/* dev계정 권한 추가하기전에  확인 */
+SHOW GRANTS FOR 'dev'@'localhost';
+
+
+/* root계정에서 실행 */
+GRANT ALL PRIVILEGES ON board.* TO 'dev'@'localhost';
+FLUSH PRIVILEGES;
+
+/* dev계정 권환 재확인 */
+SHOW GRANTS FOR 'dev'@'localhost';
