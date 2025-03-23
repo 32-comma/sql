a960bef45f7abe1190b06796c4f597a263452424
32-comma <louis.develog@gmail.com>
2025-03-23 13:45:54 +0900

    update

--- mariaDB/grant.sql
+++ mariaDB/grant.sql
@@ -3,8 +3,9 @@ SHOW GRANTS FOR 'dev'@'localhost';
 
 
 /* root계정에서 실행 */
-GRANT ALL PRIVILEGES ON board.* TO 'dev'@'localhost';
+GRANT ALL PRIVILEGES ON blog.* TO 'dev'@'localhost';
 FLUSH PRIVILEGES;
 
 /* dev계정 권환 재확인 */
 SHOW GRANTS FOR 'dev'@'localhost';
+
---
+++ mariaDB/revoke.sql
@@ -0,0 +1,1 @@
+REVOKE ALL PRIVILEGES ON board.* FROM 'username'@'host';
